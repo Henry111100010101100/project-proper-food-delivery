63764841522ee6ce315c8343372340de1ae46789
Henry <sergenovak92@gmail.com>
2022-12-21 18:59:46 +0300

    modified script.js in module system

---
+++ js/modules/modal.js
@@ -0,0 +1,50 @@
+function modal() {
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'); // Обработчики событий, повешенные на элементы полученные из html через querySelector не применятся на элементы, созданные динамически (через JS), поэтому необходимо использовать делегирование событий; 
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+    
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute('data-close') == '') {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) { 
+            closeModal();
+        }
+    });
+
+     const modalTimerId = setTimeout(openModal, 50000);
+    // Закомментировал, чтобы не отвлекало
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
+
+}
+
+module.exports = modal;
